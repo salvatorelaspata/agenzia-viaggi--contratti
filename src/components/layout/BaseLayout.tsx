e16126fd6048a3870d65849a7f3fb7d9863b497c
SalvatoreLa <salvatore.la.spata@gmail.com>
2023-03-07 22:07:48 +0100

    uninstall tailwind css and remove here config

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -4,12 +4,12 @@ import { Header } from './Header';
 const BaseLayout: React.FC<{ title: string, children: React.ReactNode }> = ({ title, children }) => {
     return (
         <div className="App">
-            <Header />
+            {/* <Header /> */}
             <main>
                 <h1 className='px-4'><span className='text-gradient'>{title}</span></h1>
                 {children}
             </main>
-            <Footer />
+            {/* <Footer /> */}
         </div>
     )
 }
