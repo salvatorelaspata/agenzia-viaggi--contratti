d2e2a4cc3abc6d61ef63ca8399f86d823a53bbc5
SalvatoreLa <salvatore.la.spata@gmail.com>
2023-03-07 22:13:32 +0100

    use mantine ui kit

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -1,10 +1,12 @@
 import { Footer } from './Footer';
 import { Header } from './Header';
+import { HeaderTabs } from './HeaderWithTabs';
 
 const BaseLayout: React.FC<{ title: string, children: React.ReactNode }> = ({ title, children }) => {
     return (
         <div className="App">
             {/* <Header /> */}
+            <HeaderTabs tabs={['Contracts', 'Projects', 'Invoices', 'Settings']} user={{ name: 'John Doe', image: 'https://avatars.githubusercontent.com/u/1443320?v=4' }} />
             <main>
                 <h1 className='px-4'><span className='text-gradient'>{title}</span></h1>
                 {children}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { Database } from '@/types/supabase'
+import { MantineProvider } from '@mantine/core'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { Session } from '@supabase/supabase-js'
@@ -13,7 +14,16 @@ export default function App({ Component, pageProps }: AppProps<{ initialSession:
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'dark',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionContextProvider>
   )
 }
